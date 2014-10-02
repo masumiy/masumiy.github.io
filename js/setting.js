5dfa8e542297d2e702afab80c2fecb5b271bc0e9
Mausmi Yamashita <masumiy@lily.ocn.ne.jp>
2014-10-02 16:24:15 +0900

    10021625 yokohama

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'mishima_2014_budget';
-OpenSpending.year = '2014';
+OpenSpending.identifier = 'yokohama_budget';
+OpenSpending.year = '2012';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
